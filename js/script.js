f3cba96930db9aeec3d7be554123ab7631d55df5
Fortunemalatji <105607746+Fortunemalatji@users.noreply.github.com>
2024-02-28 08:36:36 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+const active = document.getElementsByTagName("active");
+
+function coloract(){  
+        active.display.color = '#ec5';
+}
+
+coloract();
+
+function toggleDropdown() {
+        var dropdown = document.getElementById("myDropdown");
+        if (dropdown.style.display === "none") {
+          dropdown.style.display = "block";
+        } else {
+          dropdown.style.display = "none";
+        }
+      }
+
+function myFunction() {
+ var x = document.getElementById("myHeader");
+ if (x.className === "header-bottom") {
+ x.className += " responsive";
+} else {
+ x.className = "header-bottom";
+}
+ }
+ // JavaScript code
+document.addEventListener("DOMContentLoaded", function() {
+        var menuIcon = document.querySelector('.menu-icon');
+        var navbar = document.querySelector('.navbar');
+    
+        // Add event listener to the menu icon
+        menuIcon.addEventListener('click', function() {
+            navbar.classList.toggle('active');
+        });
+    });
+// JavaScript code
+function togglebar() {
+        var navbar = document.getElementById('navbar');
+    
+        // Toggle the visibility of the navigation menu
+        if (navbar.style.display === 'block') {
+            navbar.style.display = 'none';
+        } else {
+            navbar.style.display = 'block';
+        }
+    }
+
+    function myFunction() {
+        var x = document.getElementById("navbar");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
+        
+      document.querySelector('#contact-form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        e.target.elements.name.value = '';
+        e.target.elements.email.value = '';
+        e.target.elements.message.value = '';
+      });
+    
